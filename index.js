297bb94b5d418743a25acdc5801bd32369f28b85
Mohammed Khan <mkha5549@stu.raritanval.edu>
2025-02-07 19:48:30 -0500

    adding  a message in the index.js file and updating the package version in the package.json file.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 
+const myName = "Mohammed Khan"; 
+let myDay = "Thursday"; 
+const message = `Howdy, I'm ${myName} and my special day this year is ${myDay}`; 
 
-console.log("Mohammed Khan"); 
+console.log(message); 
+
